2b2e2d5ff72c684d0bf2d5c9d0af9110ef754b39
Anton Vlasiuk <anton.vlasiuk@external.toyota-europe.com>
2020-10-18 18:34:09 +0200

    [ Added ] Issue 2 -[ Selflog is not working ] [ NetCore ] console output

--- ConsoleApp-NetCore/Program.cs
+++ ConsoleApp-NetCore/Program.cs
@@ -1,31 +1,55 @@
 ﻿using Serilog;
-using Serilog.Core;
+using Serilog.Formatting.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace ConsoleApp_NetCore
 {
     internal class Program
     {
+        public const string HARDCODED_SERILOGDEBUG_LOCATION = @"C:\Repos\tmp\LogsTest\SerilogDebug.txt";
+        public const string HARDCODED_LOGS_LOCATION = @"C:\Repos\tmp\LogsTest\log.json";
+
         private static void Main(string[] args)
         {
-            // This one is not working.
-            // Issue 1 - LoggerConfiguration doesn't have url parameter named 'url'
-            Logger logger1 = CreateLogger1("URL", "<API_KEY>");
-        }
+            #region Issue 1 - LoggerConfiguration doesn't have url parameter named 'url'
 
-        /// <summary>
-        /// Issue 1 - LoggerConfiguration doesn't have url parameter named 'url'
-        ///
-        /// Documentation reference:
-        /// https://docs.datadoghq.com/logs/log_collection/csharp/?tab=serilog#agentless-logging
-        /// </summary>
-        /// <returns></returns>
-        private static Logger CreateLogger1(string urlParam, string apiKey)
-        {
-            var log = new LoggerConfiguration(url: urlParam)
-            .WriteTo.DatadogLogs(apiKey)
-            .CreateLogger();
+            ////Documentation reference:
+            ////https://docs.datadoghq.com/logs/log_collection/csharp/?tab=serilog#agentless-logging
 
-            return log;
+            //// This one is not working.
+            //Logger logger1 = new LoggerConfiguration(url: "URL")
+            //.WriteTo.DatadogLogs("<API_KEY>")
+            //.CreateLogger();
+
+            #endregion Issue 1 - LoggerConfiguration doesn't have url parameter named 'url'
+
+            #region Issues 2 - SelfLog is not working
+
+            var file = File.CreateText(HARDCODED_SERILOGDEBUG_LOCATION);
+
+            Serilog.Debugging.SelfLog.Enable(TextWriter.Synchronized(file));
+            Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+            Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo
+                .File(new JsonFormatter(renderMessage: true), HARDCODED_LOGS_LOCATION)
+                .Enrich.WithThreadId()
+                .Enrich.WithMachineName()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            string message = $"Issue 2 [ConsoleApp-NetCore] POC info - {DateTime.Now.ToLongTimeString()}";
+
+            logger.Warning(message);
+            logger.Information(message);
+            logger.Error(message);
+            logger.Fatal(message);
+
+            #endregion Issues 2 - SelfLog is not working
         }
 
         //private static void Main(string[] args)
@@ -101,7 +125,7 @@ namespace ConsoleApp_NetCore
         //{
         //    var logger = new LoggerConfiguration()
         //        .WriteTo
-        //        .File(new JsonFormatter(renderMessage: true), HARDCODED_LOCATION)
+        //        .File(new JsonFormatter(renderMessage: true), HARDCODED_LOGS_LOCATION)
         //        .CreateLogger();
 
         //    return logger;
--- ConsoleApp-NetFramework/Program.cs
+++ ConsoleApp-NetFramework/Program.cs
@@ -1,12 +1,47 @@
-﻿namespace ConsoleApp_NetFramework
+﻿using Serilog;
+using Serilog.Formatting.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleApp_NetFramework
 {
     internal class Program
     {
+        public const string HARDCODED_SERILOGDEBUG_LOCATION = @"C:\Repos\tmp\LogsTest\SerilogDebug.txt";
+        public const string HARDCODED_LOGS_LOCATION = @"C:\Repos\tmp\LogsTest\log.json";
+
         private static void Main(string[] args)
         {
+            #region Issues 2 - SelfLog is not working
+
+            var file = File.CreateText(HARDCODED_SERILOGDEBUG_LOCATION);
+
+            Serilog.Debugging.SelfLog.Enable(TextWriter.Synchronized(file));
+            Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+            Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo
+                .File(new JsonFormatter(renderMessage: true), HARDCODED_LOGS_LOCATION)
+                .Enrich.WithThreadId()
+                .Enrich.WithMachineName()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            string message = $"Issue 2 [ConsoleApp-NetCore] POC info - {DateTime.Now.ToLongTimeString()}";
+
+            logger.Warning(message);
+            logger.Information(message);
+            logger.Error(message);
+            logger.Fatal(message);
+
+            #endregion Issues 2 - SelfLog is not working
+
             //// This one is not working.
-            //// LoggerConfiguration doesn't have url parameter named 'url'
-            //Logger logger1 = CreateLogger1("http-intake.logs.datadoghq.eu", "<API_KEY>");
+            //// Issue 1 - LoggerConfiguration doesn't have url parameter named 'url'
+            //Logger logger1 = CreateLogger1("URL", "<API_KEY>");
         }
 
         ///// <summary>
