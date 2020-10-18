88686c7e12350d38dbcafdd5d7edd79f706fe91e
Anton Vlasiuk <anton.vlasiuk@external.toyota-europe.com>
2020-10-18 17:44:33 +0200

    [ Added ] [ proofs ] LoggerConfiguration doesn't have url parameter named 'url'.png

--- ConsoleApp-NetCore/Program.cs
+++ ConsoleApp-NetCore/Program.cs
@@ -1,87 +1,110 @@
-﻿using Serilog.Core;
+﻿using Serilog;
+using Serilog.Core;
 
 namespace ConsoleApp_NetCore
 {
     internal class Program
     {
-        public const string custom_apiKey = "";
-
-        public const string custom_source = "";
-        public const string custom_host = " ";
-        public const string custom_service = "";
-
-        public static string[] custom_tags = new string[] { "ENV:DEVELOP" };
-
         private static void Main(string[] args)
         {
-            var test1 = InitDataDogLoggerWithoutAppsettings();
-            var test2 = InitDataDogLogger(configuration);
-            var test3 = InitDataDogLoggerOverride();
-
-            test1.Information("Successfully configured agentless logging 1");
-            test2.Information("Successfully configured agentless logging 2");
-            test3.Information("Successfully configured agentless logging 3");
+            // This one is not working.
+            // Issue 1 - LoggerConfiguration doesn't have url parameter named 'url'
+            Logger logger1 = CreateLogger1("URL", "<API_KEY>");
         }
 
-        private static Logger InitDataDogLoggerWithoutAppsettings()
+        /// <summary>
+        /// Issue 1 - LoggerConfiguration doesn't have url parameter named 'url'
+        ///
+        /// Documentation reference:
+        /// https://docs.datadoghq.com/logs/log_collection/csharp/?tab=serilog#agentless-logging
+        /// </summary>
+        /// <returns></returns>
+        private static Logger CreateLogger1(string urlParam, string apiKey)
         {
-            string url = "datadoghq.eu";
-
-            var config = new DatadogConfiguration(
-                url: url,
-                port: 443,
-                useSSL: true,
-                useTCP: true);
-
-            var logger = new LoggerConfiguration()
-                .WriteTo
-                .DatadogLogs(
-                    apiKey: custom_apiKey,
-                    source: custom_source,
-                    service: custom_service,
-                    host: custom_host,
-                    tags: custom_tags,
-                    configuration: config
-                )
-                .CreateLogger();
-
-            return logger;
-        }
-
-        private static Logger InitDataDogLoggerOverride()
-        {
-            var config = new DatadogConfiguration(url: "tcp-intake.logs.datadoghq.eu", port: 443, useSSL: true, useTCP: true);
-            var log = new LoggerConfiguration()
-                .WriteTo.DatadogLogs(
-                    apiKey: custom_apiKey,
-                    source: custom_source,
-                    service: custom_service,
-                    host: custom_host,
-                    tags: custom_tags,
-                    configuration: config
-                )
-                .CreateLogger();
+            var log = new LoggerConfiguration(url: urlParam)
+            .WriteTo.DatadogLogs(apiKey)
+            .CreateLogger();
 
             return log;
         }
 
-        private static Logger InitFileLogger()
-        {
-            var logger = new LoggerConfiguration()
-                .WriteTo
-                .File(new JsonFormatter(renderMessage: true), HARDCODED_LOCATION)
-                .CreateLogger();
+        //private static void Main(string[] args)
+        //{
+        //    //not working
+        //    //Logger logger1 = CreateLogger1("http-intake.logs.datadoghq.eu", custom_apiKey);
 
-            return logger;
-        }
+        //    Logger test1 = InitDataDogLoggerWithoutAppsettings();
+        //    Logger test2 = InitDataDogLoggerOverride();
 
-        private static Logger InitDataDogLogger(IConfiguration configuration)
-        {
-            var dataDogLogger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
+        //    test1.Information("Successfully configured agentless logging 1");
+        //    test2.Information("Successfully configured agentless logging 1");
+        //}
 
-            return dataDogLogger;
-        }
+        ///// <summary>
+        ///// https://docs.datadoghq.com/logs/log_collection/csharp/?tab=serilog#agentless-logging
+        /////
+        /////
+        ///// </summary>
+        ///// <returns></returns>
+        //private static object CreateLogger1(string urlParam, string apiKey)
+        //{
+        //    Logger log = new LoggerConfiguration(url: urlParam)
+        //        .WriteTo.DatadogLogs("<API_KEY>")
+        //        .CreateLogger();
+
+        //    return log;
+        //}
+
+        //private static Logger InitDataDogLoggerWithoutAppsettings()
+        //{
+        //    string url = "datadoghq.eu";
+
+        //    var config = new DatadogConfiguration(
+        //        url: url,
+        //        port: 443,
+        //        useSSL: true,
+        //        useTCP: true);
+
+        //    var logger = new LoggerConfiguration()
+        //        .WriteTo
+        //        .DatadogLogs(
+        //            apiKey: custom_apiKey,
+        //            source: custom_source,
+        //            service: custom_service,
+        //            host: custom_host,
+        //            tags: custom_tags,
+        //            configuration: config
+        //        )
+        //        .CreateLogger();
+
+        //    return logger;
+        //}
+
+        //private static Logger InitDataDogLoggerOverride()
+        //{
+        //    var config = new DatadogConfiguration(url: "tcp-intake.logs.datadoghq.eu", port: 443, useSSL: true, useTCP: true);
+        //    var log = new LoggerConfiguration()
+        //        .WriteTo.DatadogLogs(
+        //            apiKey: custom_apiKey,
+        //            source: custom_source,
+        //            service: custom_service,
+        //            host: custom_host,
+        //            tags: custom_tags,
+        //            configuration: config
+        //        )
+        //        .CreateLogger();
+
+        //    return log;
+        //}
+
+        //private static Logger InitFileLogger()
+        //{
+        //    var logger = new LoggerConfiguration()
+        //        .WriteTo
+        //        .File(new JsonFormatter(renderMessage: true), HARDCODED_LOCATION)
+        //        .CreateLogger();
+
+        //    return logger;
+        //}
     }
 }
